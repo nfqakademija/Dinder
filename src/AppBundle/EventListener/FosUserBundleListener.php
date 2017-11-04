025b75decf3917c1ff46cc763c37695bee0cffdd
MariusP <marius.repiv@gmail.com>
2017-11-04 10:29:11 +0200

    fix<FosUserBundleListener> method return type add

--- src/AppBundle/EventListener/FosUserBundleListener.php
+++ src/AppBundle/EventListener/FosUserBundleListener.php
@@ -20,7 +20,7 @@ class FosUserBundleListener implements EventSubscriberInterface
     /**
      * {@inheritdoc}
      */
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
             FOSUserEvents::RESETTING_RESET_SUCCESS => 'onFosEventSuccess',
