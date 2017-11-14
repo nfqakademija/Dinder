a4866edfddee5cdd9a5f7d56706e5eea674db882
Marius <marius.repiv@gmail.com>
2017-11-14 21:24:30 +0200

    fix<delete-fixtures-images.sh>: Fix in fixtures images delete script

--- load-fixtures.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf web/images/items/
-mkdir web/images/items/
-docker-compose exec fpm bash -c "bin/console doc:schema:drop --force && bin/console doc:schema:create && bin/console hautelook:fixtures:load"
