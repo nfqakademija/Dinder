7dc819978300f0409fabf1151e2c94a36d5cd2ca
Sergej Voronov <sergej@voronov.lt>
2017-11-24 13:23:03 +0200

    fix<.travis.yml>: add permissions for docker-compose to work properly

--- provision.sh
+++ provision.sh
@@ -2,17 +2,17 @@
 
 cp .env.dist .env
 docker-compose up -d
-composer install --prefer-dist -n
-npm install
-docker-compose exec fpm bin/console doc:database:drop --force
-docker-compose exec fpm bin/console doc:database:create
-docker-compose exec fpm bin/console doc:schema:update --force
+docker-compose exec -u 1000 fpm composer install --prefer-dist -n
+docker-compose run -u 1000 npm npm install
+docker-compose exec -u 1000 fpm bin/console doc:database:drop --force
+docker-compose exec -u 1000 fpm bin/console doc:database:create
+docker-compose exec -u 1000 fpm bin/console doc:schema:update --force
 echo -e "Generating project fixtures. It may take some time..."
 if [ -d web/images/items ]; then
-    docker-compose exec fpm rm -rf web/images/items/
+    docker-compose exec -u 1000 fpm rm -rf web/images/items/
 fi
-docker-compose exec fpm mkdir -p web/images/items/
-docker-compose exec fpm bin/console hautelook:fixtures:load --no-interaction 
+docker-compose exec -u 1000 fpm mkdir -p web/images/items/
+docker-compose exec -u 1000 fpm bin/console hautelook:fixtures:load --no-interaction
 echo -e "\n\e[30;48;5;82m                                                   \e[0m"
 echo -e "\e[30;48;5;82m  [SUCCESS] DINDER Project is READY to launch! ;)  \e[0m"
 echo -e "\e[30;48;5;82m                                                   \e[0m\n"
