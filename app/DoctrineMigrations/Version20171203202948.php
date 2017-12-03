bd1584fd9df5c0cbdb5a6ad104ea65378004e08a
skirmantas <skirmantas@splius.lt>
2017-12-03 22:31:16 +0200

    upd<User><Location><Category>: Add DB migrations.

---
+++ app/DoctrineMigrations/Version20171203202948.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20171203202948 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE fos_user ADD deleted_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE location ADD deleted_at DATETIME DEFAULT NULL');
+        $this->addSql('ALTER TABLE category ADD deleted_at DATETIME DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE category DROP deleted_at');
+        $this->addSql('ALTER TABLE fos_user DROP deleted_at');
+        $this->addSql('ALTER TABLE location DROP deleted_at');
+    }
+}
