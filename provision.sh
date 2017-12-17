d0b496b9fed07172406121d2d0f8179de410140e
skirmantas <skirmantas@splius.lt>
2017-12-17 21:31:49 +0200

    dev<Fixtures>: demo images to webpack.

--- provision.sh
+++ provision.sh
@@ -5,7 +5,6 @@ if [[ ! -f .env ]]; then
 fi
 docker-compose up -d
 docker-compose exec fpm composer install --prefer-dist -n
-docker-compose run --rm npm bash -c "npm install && ./node_modules/.bin/webpack"
 if [[ $1 == '--schema' ]]; then
     docker-compose exec fpm bin/console doc:database:drop --force
     docker-compose exec fpm bin/console doc:database:create
@@ -19,6 +18,7 @@ if [[ $1 == '--schema' ]]; then
         docker-compose exec fpm bin/console hautelook:fixtures:load --no-interaction
     fi
 fi
+docker-compose run --rm npm bash -c "npm install && ./node_modules/.bin/webpack"
 echo -e "\n\e[30;48;5;82m                                                   \e[0m"
 echo -e "\e[30;48;5;82m  [SUCCESS] DINDER Project is READY to launch! ;)  \e[0m"
 echo -e "\e[30;48;5;82m                                                   \e[0m\n"
